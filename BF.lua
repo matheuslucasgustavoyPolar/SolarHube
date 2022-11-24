1cadc0d45b7862741359cc09d9ce3028f8624626
yPolar7 <111068845+yPolar7@users.noreply.github.com>
2022-11-24 19:52:11 -0300

    fixing bugs

--- BF.lua
+++ BF.lua
@@ -2464,37 +2464,17 @@ if IKAI then
 			end
 		end)
 	
-	do  local ui =  game:GetService("CoreGui"):FindFirstChild("RippleFPS")  if ui then ui:Destroy() end
-	local uix =  game:GetService("CoreGui"):FindFirstChild("RippleOpenClose")  if uix then uix:Destroy() end
-	local uix =  game:GetService("CoreGui"):FindFirstChild("Rippletime")  if uix then uix:Destroy() end end
+	do local uix =  game:GetService("CoreGui"):FindFirstChild("RippleOpenClose")  if uix then uix:Destroy() end end
 	
 	
-	local RippleFPS = Instance.new("ScreenGui")
-	local Rippletime = Instance.new("ScreenGui")
 	local RippleOpenClose = Instance.new("ScreenGui")
-	local Framefps = Instance.new("Frame")
-	local Frametime = Instance.new("Frame")
 	local OpenClose = Instance.new("TextButton")
 	local UICorner213 = Instance.new("UICorner")
 	local UICorner214 = Instance.new("UICorner")
 	local UICorner215 = Instance.new("UICorner")
-	local TextLabelfps = Instance.new("TextLabel")
-	local TextLabeltime = Instance.new("TextLabel")
-	local ImageLabelfps = Instance.new("ImageLabel")
-	local ImageLabeltime = Instance.new("ImageLabel")
 	-- local ImageOpenClose = Instance.new("ImageLabel")
-	local Strokefps = Instance.new("UIStroke")
-	local Stroketime = Instance.new("UIStroke")
 	local StrokeOpenClose = Instance.new("UIStroke")
 	
-	RippleFPS.Name = "RippleFPS"
-	RippleFPS.Parent = game.CoreGui
-	RippleFPS.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
-	
-	Rippletime.Name = "Rippletime"
-	Rippletime.Parent = game.CoreGui
-	Rippletime.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
-
 	RippleOpenClose.Name = "RippleOpenClose"
 	RippleOpenClose.Parent = game.CoreGui
 	RippleOpenClose.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
@@ -2518,195 +2498,6 @@ if IKAI then
 	StrokeOpenClose.Color = _G.Color
 	StrokeOpenClose.Transparency = 0
 
-	Framefps.Name = "Framefps"
-	Framefps.Parent = RippleFPS
-	Framefps.BackgroundColor3 = Color3.fromRGB(35, 35, 35)
-	Framefps.BorderSizePixel = 0
-	Framefps.Position = UDim2.new(0.010, 0, 0.21084006, 0)
-	Framefps.Size = UDim2.new(0, 193, 0, 44)
-	
-	UICorner213.CornerRadius = UDim.new(0, 4)
-	UICorner213.Parent = Framefps
-	
-	Strokefps.Thickness = 1
-	Strokefps.Name = ""
-	Strokefps.Parent = Framefps
-	Strokefps.ApplyStrokeMode = Enum.ApplyStrokeMode.Border
-	Strokefps.LineJoinMode = Enum.LineJoinMode.Round
-	Strokefps.Color = _G.Color
-	Strokefps.Transparency = 0
-	
-	Frametime.Name = "Frametime"
-	Frametime.Parent = Rippletime
-	Frametime.BackgroundColor3 = Color3.fromRGB(35, 35, 35)
-	Frametime.BorderSizePixel = 0
-	Frametime.Position = UDim2.new(0.010, 0, 0.11084006, 0)
-	Frametime.Size = UDim2.new(0, 293, 0, 44)
-	
-	UICorner214.CornerRadius = UDim.new(0, 4)
-	UICorner214.Parent = Frametime
-	
-	Stroketime.Thickness = 1
-	Stroketime.Name = ""
-	Stroketime.Parent = Frametime
-	Stroketime.ApplyStrokeMode = Enum.ApplyStrokeMode.Border
-	Stroketime.LineJoinMode = Enum.LineJoinMode.Round
-	Stroketime.Color = _G.Color
-	Stroketime.Transparency = 0
-	
-	TextLabelfps.Name = "TextLabelfps"
-	TextLabelfps.Parent = Framefps
-	TextLabelfps.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-	TextLabelfps.BackgroundTransparency = 1.000
-	TextLabelfps.Position = UDim2.new(0.356857866, 0, 0.234848887, 0)
-	TextLabelfps.Size = UDim2.new(0, 124, 0, 23)
-	TextLabelfps.Font = Enum.Font.GothamSemibold
-	TextLabelfps.Text = "FPS:N/A"
-	TextLabelfps.TextColor3 = Color3.fromRGB(255, 255, 255)
-	TextLabelfps.TextSize = 14.000
-	TextLabelfps.TextXAlignment = Enum.TextXAlignment.Left
-	
-	TextLabeltime.Name = "TextLabeltime"
-	TextLabeltime.Parent = Frametime
-	TextLabeltime.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-	TextLabeltime.BackgroundTransparency = 1.000
-	TextLabeltime.Position = UDim2.new(0.356857866, 0, 0.234848887, 0)
-	TextLabeltime.Size = UDim2.new(0, -35, 0, 23)
-	TextLabeltime.Font = Enum.Font.GothamSemibold
-	TextLabeltime.Text = "FPS:N/A"
-	TextLabeltime.TextColor3 = Color3.fromRGB(255, 255, 255)
-	TextLabeltime.TextSize = 14.000
-	TextLabeltime.TextXAlignment = Enum.TextXAlignment.Left
-	
-	spawn(function()
-		while wait() do
-			pcall(function()
-				local scripttime=game.Workspace.DistributedGameTime
-				local seconds = scripttime%60
-				local minutes = math.floor(scripttime/60%60)
-				local hours = math.floor(scripttime/3600)
-				local tempo = string.format("%.0f Hour , %.0f Minute , %.0f Second", hours ,minutes, seconds) --Hours:"..os.date("%H")..":"..os.date("%M")..":"..os.date("%S")
-				TextLabeltime.Text = "Hours: "..os.date("%H")..":"..os.date("%M")..":"..os.date("%S")
-				--TextLabeltime.Text = tempo
-			end)
-		end
-	end)
-	
-	spawn(function()
-		while wait() do
-			pcall(function()
-				TextLabelfps.Text = "FPS: "..string.format("%d",workspace:GetRealPhysicsFPS())
-			end)
-		end
-	end)
-	
-	ImageLabelfps.Name = "ImageLabelfps"
-	ImageLabelfps.Parent = Framefps
-	ImageLabelfps.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-	ImageLabelfps.BackgroundTransparency = 1.000
-	ImageLabelfps.Position = UDim2.new(0.083989636, 0, 0.15545856, 0)
-	ImageLabelfps.Size = UDim2.new(0, 29, 0, 29)
-	ImageLabelfps.Image = "rbxassetid://9606070311"
-
-	ImageLabeltime.Name = "ImageLabeltime"
-	ImageLabeltime.Parent = Frametime
-	ImageLabeltime.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-	ImageLabeltime.BackgroundTransparency = 1.000
-	ImageLabeltime.Position = UDim2.new(0.083989636, -9, 0.15545856, 0)
-	ImageLabeltime.Size = UDim2.new(0, 29, 0, 29)
-	ImageLabeltime.Image = "rbxassetid://9606070311"
-	
-	
-	Framefps.MouseEnter:Connect(function()
-		TweenService:Create(
-			Framefps,
-			TweenInfo.new(0.4,Enum.EasingStyle.Quad,Enum.EasingDirection.Out),
-			{BackgroundTransparency = 0}
-		):Play()
-		TweenService:Create(
-			Strokefps,
-			TweenInfo.new(0.4,Enum.EasingStyle.Quad,Enum.EasingDirection.Out),
-			{Transparency = 0}
-		):Play()
-		TweenService:Create(
-			TextLabelfps,
-			TweenInfo.new(0.4,Enum.EasingStyle.Quad,Enum.EasingDirection.Out),
-			{TextTransparency = 0}
-		):Play()
-		TweenService:Create(
-			ImageLabelfps,
-			TweenInfo.new(0.4,Enum.EasingStyle.Quad,Enum.EasingDirection.Out),
-			{ImageTransparency = 0}
-		):Play()
-	end)
-	Framefps.MouseLeave:Connect(function()
-		TweenService:Create(
-			Framefps,
-			TweenInfo.new(0.4,Enum.EasingStyle.Quad,Enum.EasingDirection.Out),
-			{BackgroundTransparency = 0}
-		):Play()
-		TweenService:Create(
-			Strokefps,
-			TweenInfo.new(0.4,Enum.EasingStyle.Quad,Enum.EasingDirection.Out),
-			{Transparency = 0}
-		):Play()
-		TweenService:Create(
-			TextLabelfps,
-			TweenInfo.new(0.4,Enum.EasingStyle.Quad,Enum.EasingDirection.Out),
-			{TextTransparency = 0}
-		):Play()
-		TweenService:Create(
-			ImageLabelfps,
-			TweenInfo.new(0.4,Enum.EasingStyle.Quad,Enum.EasingDirection.Out),
-			{ImageTransparency = 0}
-		):Play()
-	end)
-	
-	Frametime.MouseEnter:Connect(function()
-		TweenService:Create(
-			Frametime,
-			TweenInfo.new(0.4,Enum.EasingStyle.Quad,Enum.EasingDirection.Out),
-			{BackgroundTransparency = 0}
-		):Play()
-		TweenService:Create(
-			Stroketime,
-			TweenInfo.new(0.4,Enum.EasingStyle.Quad,Enum.EasingDirection.Out),
-			{Transparency = 0}
-		):Play()
-		TweenService:Create(
-			TextLabeltime,
-			TweenInfo.new(0.4,Enum.EasingStyle.Quad,Enum.EasingDirection.Out),
-			{TextTransparency = 0}
-		):Play()
-		TweenService:Create(
-			ImageLabeltime,
-			TweenInfo.new(0.4,Enum.EasingStyle.Quad,Enum.EasingDirection.Out),
-			{ImageTransparency = 0}
-		):Play()
-	end)
-	Frametime.MouseLeave:Connect(function()
-		TweenService:Create(
-			Frametime,
-			TweenInfo.new(0.4,Enum.EasingStyle.Quad,Enum.EasingDirection.Out),
-			{BackgroundTransparency = 0}
-		):Play()
-		TweenService:Create(
-			Stroketime,
-			TweenInfo.new(0.4,Enum.EasingStyle.Quad,Enum.EasingDirection.Out),
-			{Transparency = 0}
-		):Play()
-		TweenService:Create(
-			TextLabeltime,
-			TweenInfo.new(0.4,Enum.EasingStyle.Quad,Enum.EasingDirection.Out),
-			{TextTransparency = 0}
-		):Play()
-		TweenService:Create(
-			ImageLabeltime,
-			TweenInfo.new(0.4,Enum.EasingStyle.Quad,Enum.EasingDirection.Out),
-			{ImageTransparency = 0}
-		):Play()
-	end)
-
 	OpenClose.MouseEnter:Connect(function()
 		TweenService:Create(
 			OpenClose,
@@ -2722,11 +2513,6 @@ if IKAI then
 			OpenClose,
 			TweenInfo.new(0.4,Enum.EasingStyle.Quad,Enum.EasingDirection.Out),
 			{TextTransparency = 0}
-		):Play()
-		TweenService:Create(
-			ImageOpenClose,
-			TweenInfo.new(0.4,Enum.EasingStyle.Quad,Enum.EasingDirection.Out),
-			{ImageTransparency = 0}
 		):Play()
 	end)
 	OpenClose.MouseLeave:Connect(function()
@@ -2745,38 +2531,33 @@ if IKAI then
 			TweenInfo.new(0.4,Enum.EasingStyle.Quad,Enum.EasingDirection.Out),
 			{TextTransparency = 0}
 		):Play()
-		TweenService:Create(
-			ImageOpenClose,
-			TweenInfo.new(0.4,Enum.EasingStyle.Quad,Enum.EasingDirection.Out),
-			{ImageTransparency = 0}
-		):Play()
 	end)
 	
-	Yedhee.TextTransparency = 1
-		Uitoggle.TextTransparency = 1
-		ToggleImgUi.ImageTransparency = 1
-		ToggleFrameUi.BackgroundTransparency = 1.000
-		SearchStroke.Transparency = 1
-		TweenService:Create(
-			Framefps,
-			TweenInfo.new(0,Enum.EasingStyle.Quad,Enum.EasingDirection.Out),
-			{BackgroundTransparency = 1}
-		):Play()
-		TweenService:Create(
-			Strokefps,
-			TweenInfo.new(0,Enum.EasingStyle.Quad,Enum.EasingDirection.Out),
-			{Transparency = 1}
-		):Play()
-		TweenService:Create(
-			TextLabelfps,
-			TweenInfo.new(0,Enum.EasingStyle.Quad,Enum.EasingDirection.Out),
-			{TextTransparency = 1}
-		):Play()
-		TweenService:Create(
-			ImageLabelfps,
-			TweenInfo.new(0,Enum.EasingStyle.Quad,Enum.EasingDirection.Out),
-			{ImageTransparency = 1}
-		):Play()
+	-- Yedhee.TextTransparency = 1
+	-- 	Uitoggle.TextTransparency = 1
+	-- 	ToggleImgUi.ImageTransparency = 1
+	-- 	ToggleFrameUi.BackgroundTransparency = 1.000
+	-- 	SearchStroke.Transparency = 1
+	-- 	TweenService:Create(
+	-- 		Framefps,
+	-- 		TweenInfo.new(0,Enum.EasingStyle.Quad,Enum.EasingDirection.Out),
+	-- 		{BackgroundTransparency = 1}
+	-- 	):Play()
+	-- 	TweenService:Create(
+	-- 		Strokefps,
+	-- 		TweenInfo.new(0,Enum.EasingStyle.Quad,Enum.EasingDirection.Out),
+	-- 		{Transparency = 1}
+	-- 	):Play()
+	-- 	TweenService:Create(
+	-- 		TextLabelfps,
+	-- 		TweenInfo.new(0,Enum.EasingStyle.Quad,Enum.EasingDirection.Out),
+	-- 		{TextTransparency = 1}
+	-- 	):Play()
+	-- 	TweenService:Create(
+	-- 		ImageLabelfps,
+	-- 		TweenInfo.new(0,Enum.EasingStyle.Quad,Enum.EasingDirection.Out),
+	-- 		{ImageTransparency = 1}
+	-- 	):Play()
 	
 		UserInputService.InputBegan:Connect(function(input)
 			if input.KeyCode == Enum.KeyCode[yoo] then
@@ -2853,27 +2634,6 @@ if IKAI then
 				end
 			end
 		end)
-	
-		TweenService:Create(
-			Frametime,
-			TweenInfo.new(0,Enum.EasingStyle.Quad,Enum.EasingDirection.Out),
-			{BackgroundTransparency = 1}
-		):Play()
-		TweenService:Create(
-			Stroketime,
-			TweenInfo.new(0,Enum.EasingStyle.Quad,Enum.EasingDirection.Out),
-			{Transparency = 1}
-		):Play()
-		TweenService:Create(
-			TextLabeltime,
-			TweenInfo.new(0,Enum.EasingStyle.Quad,Enum.EasingDirection.Out),
-			{TextTransparency = 1}
-		):Play()
-		TweenService:Create(
-			ImageLabeltime,
-			TweenInfo.new(0,Enum.EasingStyle.Quad,Enum.EasingDirection.Out),
-			{ImageTransparency = 1}
-		):Play()
 	
 		UserInputService.InputBegan:Connect(function(input)
 			if input.KeyCode == Enum.KeyCode[yoo] then
@@ -2977,8 +2737,6 @@ if IKAI then
 	end)
 
 		MakeDraggable(ToggleFrameUi,ToggleFrameUi)
-		MakeDraggable(Frametime,Frametime)
-		MakeDraggable(Framefps,Framefps)
 		MakeDraggable(OpenClose, OpenClose)
 	
 		local Tab = Instance.new("Frame")
